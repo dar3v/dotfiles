b7400cbe634701b2e8dc4c569a01a0acb5b634ef
dar3v <un.finished342@slmail.me>
2025-01-16 21:20:29 +0800

    move `.config` to `rice/river_rice/`

--- .config/fish/functions/lock.fish
+++
@@ -1,4 +0,0 @@
-function lock --wraps='systemctl suspend && waylock' --description 'alias lock=systemctl suspend && waylock'
-  systemctl suspend && waylock $argv
-        
-end
--- .config/fish/functions/treel.fish
+++
@@ -1,4 +0,0 @@
-function treel --wraps='tree -L 2' --description 'alias treel=tree -L 2'
-  tree -L 2 $argv
-        
-end
