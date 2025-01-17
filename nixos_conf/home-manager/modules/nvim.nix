f59b42e03c45aaf4f1249b3a036435f6c64ca9a3
dar3v <un.finished342@slmail.me>
2025-01-17 22:47:30 +0800

    nvim

--- nixos_conf/home-manager/modules/nvim.nix
+++ nixos_conf/home-manager/modules/nvim.nix
@@ -1,11 +1,19 @@
-{ config, pkgs, ...}:
+{ pkgs, ...}:
 {
 	programs.neovim = {
 		enable = true;
 		defaultEditor = true;
 
-		plugins = [
-			pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+		plugins = with pkgs.vimPlugins; [
+			nvim-treesitter.withAllGrammars
+		];
+
+		extraPackages =	with pkgs;
+		[
+			lua-language-server
+
+			nixd
+			alejandra
 		];
 	};
 }
