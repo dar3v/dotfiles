25c3b15aa968b476e2960fb70f34b0c25a5ffb5b
dar3v <un.finished342@slmail.me>
2025-01-19 16:15:31 +0800

    added `ripgrep` on `nvim.nix`

--- nixos_conf/home-manager/modules/nvim.nix
+++ nixos_conf/home-manager/modules/nvim.nix
@@ -3,17 +3,22 @@
 	programs.neovim = {
 		enable = true;
 		defaultEditor = true;
-
+	
 		plugins = with pkgs.vimPlugins; [
 			nvim-treesitter.withAllGrammars
 		];
-
+	
 		extraPackages =	with pkgs;
 		[
 			lua-language-server
-
+	
 			nixd
 			alejandra
 		];
 	};
+	
+	 home.packages = with pkgs;
+	 [
+	 	ripgrep
+	 ];
 }
