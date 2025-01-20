c24daad26afdb8f4309d3d41c85afdc07fb83023
dar3v <un.finished342@slmail.me>
2025-01-20 21:25:40 +0800

    chore: autoformat

--- nixos_conf/flake.nix
+++ nixos_conf/flake.nix
@@ -3,58 +3,56 @@
 
   # > inputs <
   inputs = {
-      # enable nixos-unstable
-      nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # enable nixos-unstable
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-      # nixos-hardware
-      nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    # nixos-hardware
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-      # home-manager
-      home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    # home-manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-      # auto-cpufreq
-      auto-cpufreq = {
-        url = "github:AdnanHodzic/auto-cpufreq";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    # auto-cpufreq
+    auto-cpufreq = {
+      url = "github:AdnanHodzic/auto-cpufreq";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-      # aagl
-      aagl = {
-        url = "github:ezKEa/aagl-gtk-on-nix";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    # aagl
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   # > outputs <
-  outputs = { self,
-              nixpkgs,
-              home-manager,
-              nixos-hardware,
-              auto-cpufreq,
-              aagl,
-              }: 
-	{
-      # nix configuration
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules =
-          [
-              ./nixos/configuration.nix
+  outputs = {
+    nixpkgs,
+    home-manager,
+    nixos-hardware,
+    auto-cpufreq,
+    aagl,
+  }: {
+    # nix configuration
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./nixos/configuration.nix
 
-              # NOTE: change accordingly to ur hardware
-              nixos-hardware.nixosModules.asus-fa506ic
-              auto-cpufreq.nixosModules.default
-              aagl.nixosModules.default
-          ];
-      };
+        # NOTE: change accordingly to ur hardware
+        nixos-hardware.nixosModules.asus-fa506ic
+        auto-cpufreq.nixosModules.default
+        aagl.nixosModules.default
+      ];
+    };
 
-      # home-manager
-      homeConfigurations.dar3v = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages."x86_64-linux";
-          modules = [ ./home-manager/home.nix ];
-	};
+    # home-manager
+    homeConfigurations.dar3v = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      modules = [./home-manager/home.nix];
+    };
   };
 }
