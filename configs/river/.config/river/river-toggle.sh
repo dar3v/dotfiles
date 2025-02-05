506768c99f54c3cf98f7244cbcbb1749fb116a1e
dar3v <un.finished342@slmail.me>
2025-02-05 17:41:46 +0800

    river: better mode toggle script

--- configs/river/.config/river/river-toggle.sh
+++ configs/river/.config/river/river-toggle.sh
@@ -1,12 +1,44 @@
 #!/bin/sh
-TOGGLE=$HOME/.config/river/.modetoggle
 
-if [ ! -e $TOGGLE ]; then
-  touch $TOGGLE
-  riverctl rule-add -app-id '*' float
-  notify-send "river" "Floating Mode"
-else
-  rm $TOGGLE
-  riverctl rule-del -app-id '*' float
-  notify-send "river" "Tiling Mode"
+set -ueo pipefail
+
+RIVER_MODE="/tmp/river_mode.txt"
+
+if [ ! -f $RIVER_MODE ]; then
+  echo "tile" > $RIVER_MODE
 fi
+
+init() {
+  riverctl rule-add -app-id '*' float
+  notify-send -a river "floating mode"
+  echo "tile" > $RIVER_MODE
+}
+
+toggle() {
+  CURRENT_MODE=$(cat "$RIVER_MODE")
+  case "$CURRENT_MODE" in
+    tile)
+      riverctl rule-del -app-id '*' float
+      notify-send -a river "tiling mode"
+      echo "float" > $RIVER_MODE
+      ;;
+    float)
+      riverctl rule-add -app-id '*' float
+      notify-send -a river "floating mode"
+      echo "tile" > $RIVER_MODE
+      ;;
+    *)
+      riverctl rule-add -app-id '*' float
+      notify-send -u critical -a river \
+        "something unexpected happened." \
+        "switching to tiling mode."
+      echo 'float' > "$RIVER_MODE"
+      ;;
+  esac
+}
+
+case "${1:-}" in
+  -init) init ;;
+  -toggle) toggle ;;
+  *) echo "usage: $0 [-init|-toggle]" ;;
+esac
