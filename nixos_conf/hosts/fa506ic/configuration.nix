f23fe75c2c4b105b804cbe95ffd20d8d55e27450
dar3v <un.finished342@slmail.me>
2025-03-19 17:37:26 +0800

    nixos: forgor some defaults

--- nixos_conf/hosts/fa506ic/configuration.nix
+++ nixos_conf/hosts/fa506ic/configuration.nix
@@ -15,6 +15,34 @@
   wm.enable = true;
   gaming = true;
 
+  #
+  # =- defaults -= #
+  #
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Define your hostname.
+  networking.hostName = "fa506ic";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Manila";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
